602e016dd7982a3cd9e11a2f4e7a5886bfd97646
Victoria Henriquez <mvhenriquez08@gmail.com>
2026-04-07 21:40:35 -0400

    Fix: Increase Workbox maximumFileSizeToCacheInBytes to 4MB

--- vite.config.ts
+++ vite.config.ts
@@ -172,6 +172,7 @@ const plugins = [
     },
     workbox: {
       globPatterns: ["**/*.{js,css,html,ico,png,svg,gif,jpg}"],
+      maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
       runtimeCaching: [
         {
           urlPattern: /^https:\/\/d2xsxph8kpxj0f\.cloudfront\.net\/.*/i,
